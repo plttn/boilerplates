6c355ce0db1edd80db715ab1317169fe29bae2b4
Jack Platten <plttn@users.noreply.github.com>
2015-05-17 23:27:12 -0700

    Tweak cpp boilerplate

--- cpp.cpp
+++ cpp.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main() {
 	int numeral = 5;
+	int inputNumeral;
 	std::cout << "message" << std::endl;
+	std::cin >> inputNumeral;
+	std::cout << inputNumeral << std::endl;
 	return 0;
 }
